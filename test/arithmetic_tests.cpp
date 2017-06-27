44761ff1f2357bf12fb94fd88d42f7b7ab8fddb3
Dillon Huff <dillonhuff@gmail.com>
2017-06-27 12:49:23 -0700

    Adding more polynomial test cases

--- test/arithmetic_tests.cpp
+++ test/arithmetic_tests.cpp
@@ -6,18 +6,30 @@ namespace ralg {
 
   TEST_CASE("Polynomial arithmetic") {
 
-    SECTION("Adding polynomials with no common coefficients") {
+    SECTION("Adding polynomials") {
       monomial two_x_sq({"2"}, {2, 0, 0}, 3);
+      monomial four_x_sq({"4"}, {2, 0, 0}, 3);
       monomial mthree_x_y3({"-3"}, {1, 3, 0}, 3);
 
-      polynomial p{{two_x_sq}, 3};
-      polynomial q{{mthree_x_y3}, 3};
+      SECTION("with no common coefficients") {
 
-      polynomial expected{{two_x_sq, mthree_x_y3}, 3};
+	polynomial p{{two_x_sq}, 3};
+	polynomial q{{mthree_x_y3}, 3};
 
-      polynomial res = p + q;
+	polynomial expected{{two_x_sq, mthree_x_y3}, 3};
 
-      REQUIRE(res == expected);
+	polynomial res = p + q;
+
+	REQUIRE(res == expected);
+      }
+
+      SECTION("That are the same") {
+	polynomial p({mthree_x_y3}, 3);
+
+	polynomial res = p + p;
+
+	polynomial expected({four_x_sq}, 3);
+      }
     }
   }
 
