a572a1ae8e62fdde353613f0bfe5da6b40fc6e93
Dillon Huff <dillonhuff@gmail.com>
2017-06-27 09:20:44 -0700

    Adding operator== for polynomials

--- test/division_tests.cpp
+++ test/division_tests.cpp
@@ -10,25 +10,25 @@ namespace ralg {
     monomial m1{{"1"}, {2, 1}, 2};
     monomial m2{{"1"}, {0, 1}, 2};
 
-    // polynomial f{{m1, m2}, 2};
+    polynomial f{{m1, m2}, 2};
 
-    // polynomial g1{{{1.0, {1, 1}, 2}, {1.0, {1, 0}, 2}}, 2};
-    // polynomial g2{{{1.0, {2, 0}, 2}, {1.0, {0, 0}, 2}}, 2};
+    polynomial g1{{{{"1"}, {1, 1}, 2}, {{"1"}, {1, 0}, 2}}, 2};
+    polynomial g2{{{{"1"}, {2, 0}, 2}, {{"1"}, {0, 0}, 2}}, 2};
 
-    // polynomial a1{{{1.0, {1, 0}, 2}}, 2};
-    // polynomial a2{{{-1.0, {0, 0}, 2}}, 2};
-    // polynomial r{{{-1.0, {0, 1}, 2}, {-1.0, {0, 0}, 2}}, 2};
+    polynomial a1{{{{"1"}, {1, 0}, 2}}, 2};
+    polynomial a2{{{{"-1"}, {0, 0}, 2}}, 2};
+    polynomial r{{{{"-1"}, {0, 1}, 2}, {{"-1"}, {0, 0}, 2}}, 2};
 
-    // std::vector<polynomial > as{a1, a2};
-    // division_result expected_result{as, r};    
+    std::vector<polynomial > as{a1, a2};
+    division_result expected_result{as, r};    
 
-    // SECTION("Remainder is correct") {
+    SECTION("Remainder is correct") {
 
-    //   std::vector<polynomial > g{g1, g2};
-    //   division_result res = divide(f, g, lexicographic_order);
+      std::vector<polynomial > g{g1, g2};
+      division_result res = divide(f, g, lexicographic_order);
 
-    //   REQUIRE(poly_eq(double_eq, res.remainder, expected_result.remainder));
-    // }
+      REQUIRE(res.remainder == expected_result.remainder);
+    }
 
     // SECTION("Remainder changes for ") {
     //   std::vector<polynomial > g{g2, g1};
