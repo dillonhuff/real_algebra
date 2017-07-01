a4bb4936af160f27f15a2138969a770a52d09518
Dillon Huff <dillonhuff@gmail.com>
2017-06-30 20:58:26 -0700

    Added a matrix class

---
+++ src/matrix.cpp
@@ -0,0 +1,6 @@
+#include "matrix.h"
+
+namespace ralg {
+
+  
+}
---
+++ src/matrix.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <vector>
+
+namespace ralg {
+
+  template<typename T>
+  class matrix {
+  protected:
+    std::vector<T> elems;
+    
+  public:
+    
+  };
+
+}
