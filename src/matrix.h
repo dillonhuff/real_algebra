e603caa027e1795a97c559e496789c073f73035b
Dillon Huff <dillonhuff@gmail.com>
2017-06-30 21:16:02 -0700

    Added determinant function

--- src/matrix.h
+++ src/matrix.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cassert>
 #include <vector>
 
 namespace ralg {
@@ -15,8 +16,7 @@ namespace ralg {
 
     matrix(const int p_n_rows,
 	   const int p_n_cols) :
-      n_rows(p_n_rows), n_cols(p_n_cols) {
-      elems(n_rows * n_cols);
+      n_rows(p_n_rows), n_cols(p_n_cols), elems(p_n_rows*p_n_cols) {
     }
 
     const T& get(const int r, const int c) const {
@@ -24,9 +24,22 @@ namespace ralg {
     }
 
     void set(const int r, const int c, const T& e) {
-      return elems[r + c*n_rows] = e;
+      elems[r + c*n_rows] = e;
     }
 
+    int num_rows() const { return n_rows; }
+    int num_cols() const { return n_rows; }
   };
+
+  template<typename T>
+  T determinant(const matrix<T>& m) {
+    assert(m.num_rows() == m.num_cols());
+    
+    if (m.num_rows() == 2) {
+      return m.get(0, 0) * m.get(1, 1) - m.get(0, 1)*m.get(1, 0);
+    }
+
+    assert(false);
+  }
 
 }
--- test/matrix_tests.cpp
+++ test/matrix_tests.cpp
@@ -1,9 +1,17 @@
 #include "catch.hpp"
 
+#include "matrix.h"
+
 namespace ralg {
 
   TEST_CASE("Computing the determinant") {
-    REQUIRE(false);
+    matrix<double> m(2, 2);
+    m.set(0, 0, 1);
+    m.set(0, 1, 1);
+    m.set(1, 0, 1);
+    m.set(1, 1, 1);
+
+    REQUIRE(determinant(m) == 0.0);
   }
 
 }
