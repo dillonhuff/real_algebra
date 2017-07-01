dbcbc4ca43b88eb3e2a85604acf8c1d332312023
Dillon Huff <dillonhuff@gmail.com>
2017-06-30 21:08:24 -0700

    Adding matrix tests

--- src/matrix.h
+++ src/matrix.h
@@ -7,10 +7,26 @@ namespace ralg {
   template<typename T>
   class matrix {
   protected:
+    int n_rows;
+    int n_cols;
     std::vector<T> elems;
     
   public:
-    
+
+    matrix(const int p_n_rows,
+	   const int p_n_cols) :
+      n_rows(p_n_rows), n_cols(p_n_cols) {
+      elems(n_rows * n_cols);
+    }
+
+    const T& get(const int r, const int c) const {
+      return elems[r + c*n_rows];
+    }
+
+    void set(const int r, const int c, const T& e) {
+      return elems[r + c*n_rows] = e;
+    }
+
   };
 
 }
---
+++ test/matrix_tests.cpp
@@ -0,0 +1,9 @@
+#include "catch.hpp"
+
+namespace ralg {
+
+  TEST_CASE("Computing the determinant") {
+    REQUIRE(false);
+  }
+
+}
