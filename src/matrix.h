efe0ac819109e0e0cf86d90d5d15ff8bea4bc2e9
Dillon Huff <dillonhuff@gmail.com>
2017-06-30 22:22:52 -0700

    Need to create matrix values

--- src/matrix.h
+++ src/matrix.h
@@ -3,27 +3,28 @@
 #include <cassert>
 #include <vector>
 
+#include "polynomial.h"
+
 namespace ralg {
 
-  template<typename T>
   class matrix {
   protected:
     int n_rows;
     int n_cols;
-    std::vector<T> elems;
+    std::vector<polynomial> elems;
     
   public:
 
     matrix(const int p_n_rows,
 	   const int p_n_cols) :
-      n_rows(p_n_rows), n_cols(p_n_cols), elems(p_n_rows*p_n_cols) {
+      n_rows(p_n_rows), n_cols(p_n_cols) {
     }
 
-    const T& get(const int r, const int c) const {
+    const polynomial& get(const int r, const int c) const {
       return elems[r + c*n_rows];
     }
 
-    void set(const int r, const int c, const T& e) {
+    void set(const int r, const int c, const polynomial& e) {
       elems[r + c*n_rows] = e;
     }
 
@@ -31,19 +32,18 @@ namespace ralg {
     int num_cols() const { return n_rows; }
   };
 
-  template<typename T>
-  T determinant(const matrix<T>& m) {
+  inline polynomial determinant(const matrix& m) {
     assert(m.num_rows() == m.num_cols());
     
     if (m.num_rows() == 2) {
       return m.get(0, 0) * m.get(1, 1) - m.get(0, 1)*m.get(1, 0);
     }
 
-    T determinant;
+    polynomial determinant = zero_polynomial(m.get(0, 0).num_vars());
     for (int i = 0; i < m.num_cols(); i++) {
-      matrix<T> minor_i = minor(i, m);
-      
     }
+
+    return determinant;
   }
 
 }
--- test/matrix_tests.cpp
+++ test/matrix_tests.cpp
@@ -11,31 +11,42 @@ namespace ralg {
   TEST_CASE("Computing the determinant") {
 
     SECTION("Base case: two by two matrix") {
-      matrix<double> m(2, 2);
-      m.set(0, 0, 1);
-      m.set(0, 1, 1);
-      m.set(1, 0, 1);
-      m.set(1, 1, 1);
+      matrix m(2, 2);
 
-      REQUIRE(determinant(m) == 0.0);
+      monomial one_m({"1"}, {0}, 1);
+      polynomial one({one_m}, 1);
+
+      cout << "Setting values" << endl;
+
+      m.set(0, 0, one);
+      m.set(0, 1, one);
+      m.set(1, 0, one);
+      m.set(1, 1, one);
+
+      cout << "Setting values" << endl;
+      cout << "(0, 0) = " << endl; //m.get(0, 0) << endl;
+      // polynomial det = determinant(m);
+      // cout << "Determinant = " << det << endl;
+
+      //REQUIRE(det == zero_polynomial(1));
     }
 
     SECTION("3 by 3 matrix") {
-      matrix<int> m(3, 3);
+      matrix m(3, 3);
 
-      m.set(0, 0, -3);
-      m.set(0, 1, 2);
-      m.set(0, 2, 1);
+      // m.set(0, 0, -3);
+      // m.set(0, 1, 2);
+      // m.set(0, 2, 1);
 
-      m.set(1, 0, 1);
-      m.set(1, 1, 0);
-      m.set(1, 2, 1);
+      // m.set(1, 0, 1);
+      // m.set(1, 1, 0);
+      // m.set(1, 2, 1);
 
-      m.set(2, 0, 7);
-      m.set(2, 1, 1);
-      m.set(2, 2, 9);
+      // m.set(2, 0, 7);
+      // m.set(2, 1, 1);
+      // m.set(2, 2, 9);
 
-      double det = determinant(m);
+      polynomial det = determinant(m);
 
       cout << det << endl;
       
